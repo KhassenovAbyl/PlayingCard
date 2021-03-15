78aa92bbe9e8c18ba094e83a4a503381484d36fc
KhassenovAbyl <khassenov00@icloud.com>
2021-03-15 21:00:26 +0600

    Lecture 5-6 iOS Stanford course

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var deck = PlayingCardDeck()
+    
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet{
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left , .right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+        
+    @IBAction func fliipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = false
+        default:
+            break
+        }
+    }
+    @objc func nextCard(){
+        if let card = deck.drow(){
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
